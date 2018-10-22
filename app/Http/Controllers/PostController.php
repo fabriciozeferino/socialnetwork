eb1ed049275ed2b8855f553ee58ed7d75d2dbda4
Fabricio Zeferino <fabriciozeferino@gmail.com>
2018-10-22 09:10:51 +0100

    Comment and Post Controller created

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Post::create([
+            "user_id" => $request->user_id,
+	        "img_url" => $request->img_url,
+	        "title" => $request->title
+        ]);
+        return "Post created";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Post::where('id', $id)->get();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Post::where('id', $id)
+            ->update([
+                "user_id" => $request->user_id,
+                "img_url" => $request->img_url,
+                "title" => $request->title
+            ]);
+        return "Post Updated";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+    }
+}
